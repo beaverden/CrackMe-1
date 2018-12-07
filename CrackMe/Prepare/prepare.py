a2ca4f95406c62b5a4db85dcf338c143386d3a04
Rozimovschii Denis <rozimovschii.denis@gmail.com>
2018-12-07 11:50:19 +0200

    Fixed build directory

--- CrackMe/Prepare/prepare.py
+++ CrackMe/Prepare/prepare.py
@@ -4,7 +4,7 @@ import struct
 import os
 
 def ROR(x, n):
-    mask = (2L**n) - 1
+    mask = (2**n) - 1
     mask_bits = x & mask
     return (x >> n) | (mask_bits << (8 - n))
 
@@ -17,7 +17,7 @@ pe.sections[3].Name = 'DRS 0_0'
 IMAGE_SCN_MEM_WRITE = 0x80000000
 pe.sections[0].Characteristics |= IMAGE_SCN_MEM_WRITE
 
-out_file = os.path.join(os.path.dirname(sys.argv[1]), 'CrackMe#1.exe$')
+out_file = sys.argv[2]
 pe.write(out_file)
 pe.close()
 
@@ -25,22 +25,22 @@ data = bytearray(open(out_file, 'rb').read())
 data = data[:0x4E] + 'Would it run in a Windows 7 VM?'.ljust(39, ' ') + data[0x4E+39:]
 
 ptr = pe.sections[0].PointerToRawData
-print ptr
 done = False
 while True:
     if struct.unpack('<I', data[ptr:ptr+4])[0] == 0x83EC8B55:
-        print 'Function'
+        print('Function')
         for x in range(ptr, ptr+40):
             if struct.unpack('<I', data[x:x+4])[0] == 0x13371337:
                 done = True
                 break
     if done: break
     if ''.join([chr(x) for x in data[ptr:ptr+28]]) == 'Unhandled exception occurred':
-        print 'Found exception'
+        print('Found exception')
         ptr += 28
         continue
     data[ptr] = ROR(data[ptr], 5)
     ptr += 1
 
 open(out_file, 'wb').write(data)
+print('OK')
     
